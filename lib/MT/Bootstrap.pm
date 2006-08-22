c2c69a2a5e450f53f6c48b890fe80660ce41de0d
bchoate <bchoate>
2006-08-22 22:18:48 +0000

    Added header and Id keyword.

--- lib/MT/Bootstrap.pm
+++ lib/MT/Bootstrap.pm
@@ -1,3 +1,9 @@
+# Copyright 2001-2006 Six Apart. This code cannot be redistributed without
+# permission from www.sixapart.com.  For more information, consult your
+# Movable Type license.
+#
+# $Id$
+
 package MT::Bootstrap;
 
 use strict;
