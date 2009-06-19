c01ec34f0c175fb6cd84be27c286f14792974a6e
plim <plim@3860aeb0-bd15-0410-af20-b8d1745f506e>
2009-06-19 21:34:38 +0000

    Adding a new config directive RaiseError which defaults to 1.  I also changed default behavior of SingleCommunity to 1.
    git-svn-id: http://code.sixapart.com/svn/movabletype/branches/slapshot@3895 3860aeb0-bd15-0410-af20-b8d1745f506e

--- lib/MT/Core.pm
+++ lib/MT/Core.pm
@@ -364,7 +364,8 @@ BEGIN {
                 default => 'a href,b,i,br/,p,strong,em,ul,ol,li,blockquote,pre',
             },
             'GenerateTrackBackRSS' => { default => 0, },
-
+            'RaiseError'           => { default => 1, },
+            
             ## Search settings, copied from Jay's mt-search and integrated
             ## into default config.
             'NoOverride'          => { default => '', },
@@ -530,7 +531,7 @@ BEGIN {
             'PerformanceLoggingThreshold' => { default => 0.1 },
             'ProcessMemoryCommand' => { handler => \&ProcessMemoryCommand },
             'EnableAddressBook' => { default => 0 },
-            'SingleCommunity' => { default => 0 },
+            'SingleCommunity' => { default => 1 },
             'DefaultTemplateSet' => { default => 'mt_blog' },
 
             'AssetFileTypes' => { type    => 'HASH' },
