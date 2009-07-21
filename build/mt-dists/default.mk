5e6588364a358b9046b7e3fcd1a716bab630aa79
plim <plim@3860aeb0-bd15-0410-af20-b8d1745f506e>
2009-07-21 20:00:44 +0000

    Changed emails to be 127 characters instead of 50.  Also upping the schema version of the default make stuff. BugId: 100878
    git-svn-id: http://code.sixapart.com/svn/movabletype/branches/slapshot@4046 3860aeb0-bd15-0410-af20-b8d1745f506e

--- build/mt-dists/default.mk
+++ build/mt-dists/default.mk
@@ -1,6 +1,7 @@
+<<<<<<< HEAD:build/mt-dists/default.mk
 PRODUCT_NAME = OpenMelody Core
 PRODUCT_VERSION = 0.9.1
-SCHEMA_VERSION = 4.0072
+SCHEMA_VERSION = 4.0073
 # Corresponds to MT 4.3
 API_VERSION = 4.3
 
