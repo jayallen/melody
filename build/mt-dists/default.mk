6b2ea83c6da3440e7271d829e1c26c27b9e3759a
plim <plim@3860aeb0-bd15-0410-af20-b8d1745f506e>
2009-07-23 05:29:19 +0800

    Adding upgrade function to disable Cloner plugin since its functions are part of core now.  Incrementing schema version to handle this.  BugId: 101209
    git-svn-id: http://code.sixapart.com/svn/movabletype/branches/slapshot@4068 3860aeb0-bd15-0410-af20-b8d1745f506e
    
    Signed-off-by: Open Melody Software Group <admin@openmelody.org>

--- build/mt-dists/default.mk
+++ build/mt-dists/default.mk
@@ -1,7 +1,6 @@
-<<<<<<< HEAD:build/mt-dists/default.mk
 PRODUCT_NAME = OpenMelody Core
 PRODUCT_VERSION = 0.9.1
-SCHEMA_VERSION = 4.0073
+SCHEMA_VERSION = 4.0074
 # Corresponds to MT 4.3
 API_VERSION = 4.3
 
