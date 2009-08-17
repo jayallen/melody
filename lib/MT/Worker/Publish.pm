9aac1a1a0b6fedc0a19e30f765453522979745aa
Jay Allen <jay@endevver.com>
2009-08-17 01:43:27 -0700

    [#131 state:committed] Replaced an instance in MT::Worker::Publish where the mt_sync task class name, MT::Worker::Sync, was hard coded instead of being pulled from the registry.  This eliminated the possibility of reliable overrides by plugins.

--- lib/MT/Worker/Publish.pm
+++ lib/MT/Worker/Publish.pm
@@ -42,7 +42,9 @@ sub work {
         $job_iter = sub { shift @jobs };
     }
 
-    my $sync = MT->config('SyncTarget');
+    my $task_workers = MT->registry('task_workers') || {};
+    my $sync_task    = $task_workers->{mt_sync};
+    my $sync         = MT->config('SyncTarget') if $sync_task;
 
     my $start = [gettimeofday];
     my $rebuilt = 0;
@@ -86,7 +88,7 @@ sub work {
         if (defined $res) {
             if ( $sync ) {
                 my $sync_job = TheSchwartz::Job->new();
-                $sync_job->funcname('MT::Worker::Sync');
+                $sync_job->funcname( $sync_task->{class} );
                 $sync_job->uniqkey($fi_id);
                 $sync_job->coalesce($job->coalesce) if $job->coalesce;
                 $sync_job->priority($job->priority) if $job->priority;
