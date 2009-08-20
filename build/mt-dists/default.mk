850d22c24bad92e913992bf72475ee77f6824577
mjacobs <mjacobs@3860aeb0-bd15-0410-af20-b8d1745f506e>
2009-08-20 18:28:31 +0000

    Bumping the schema and version number for the *correct* branch
    git-svn-id: http://code.sixapart.com/svn/movabletype/branches/hanson@4151 3860aeb0-bd15-0410-af20-b8d1745f506e
    
    Signed-off-by: Open Melody Software Group <admin@openmelody.org>

--- build/mt-dists/default.mk
+++ build/mt-dists/default.mk
@@ -1,6 +1,16 @@
 PRODUCT_NAME = Melody
 PRODUCT_VERSION = 0.9.4
 SCHEMA_VERSION = 4.0076
-# Corresponds to MT 4.31
-API_VERSION = 4.31
+# Corresponds to MT 4.32
+API_VERSION = 4.32
 BUILD_LANGUAGE = en_US
+VERSION_SUFFIX = 
+PORTAL_URL = http://openmelody.org/
+SUPPORT_URL = http://openmelody.org/help/
+NEWS_URL = http://openmelody.org/blog/
+NEWSBOX_URL = http://openmelody.org/blog/melody_news_widget.html
+LEARNINGNEWS_URL = http://learning.movabletype.org/newsbox.html
+CATEGORY_NAME_NODASH = 0
+DEFAULT_TIMEZONE = 0
+MAIL_ENCODING = ISO-8859-1
+PUBLISH_CHARSET = utf-8
