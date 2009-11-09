c5f9f66d0c39a421de1c9cfdb14f70aea90dba17
Byrne <byrne@majordojo.com>
2009-11-09 01:21:50 -0800

    reverting change that added addons/plugins to search path for plugins. decided correctly to just use existing search paths and move bundled plugins to addons directory.

--- lib/MT/Core.pm
+++ lib/MT/Core.pm
@@ -319,7 +319,7 @@ BEGIN {
                 path    => 1,
             },
             'PluginPath' => {
-                default => [ 'plugins' ],
+                default => 'plugins',
                 path    => 1,
                 type    => 'ARRAY',
             },
