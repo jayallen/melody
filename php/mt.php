6557ca177b319b4db547b3a6f511f9b4fb7e8bd6
Jay Allen <jay@endevver.com>
2010-08-18 12:42:38 -0700

    Bumped version number to v0.9.18

--- php/mt.php
+++ php/mt.php
@@ -7,8 +7,8 @@
 
 if('__MAKE_ME__' == '__MAKE_' . 'ME__') { # If make is not run
     define('VERSION', '4.34');
-    define('VERSION_ID', '0.9.17 (MT 4.34+)');
-    define('PRODUCT_VERSION', '0.9.17');
+    define('VERSION_ID', '0.9.18 (MT 4.34+)');
+    define('PRODUCT_VERSION', '0.9.18');
     define('PRODUCT_NAME', 'Melody');    
 } else {
     define('VERSION', '__API_VERSION__');
