1ed391fbf0b786de8ada433ecf25563a31bbf586
Byrne <byrne@majordojo.com>
2010-10-06 13:22:56 -0700

    updated POD docs

--- addons/MultiBlog.plugin/lib/MultiBlog/Melody.pm
+++ addons/MultiBlog.plugin/lib/MultiBlog/Melody.pm
@@ -36,3 +36,56 @@ sub search_blog_filter {
 
 1;
 __END__
+
+
+=head1 NAME
+
+MultiBlog::Melody - this package contains Melody-specific additions to the
+MultiBlog plugin.
+
+=head1 METHODS
+
+=over 4 search_blog_filter
+
+This method is invoked via the search_blog_list callback. It is responsible
+for modifying the list of blogs to be included and excluded from a search. 
+The callback was implemented by Melody to address the concern that the 
+Context Privacy setting associated with a blog's MultiBlog configuration
+was not being respected when executing a search.
+
+Without this callback, one could configure a blog to be "private" but still
+have that blog's content surface in search results - thus violating the 
+intent of the setting. 
+
+Users should be warned that Six Apart advises the following with regards to
+this capability:
+
+1) The prospective behavior (having mt-search respect Multiblog privacy
+settings) was never an intended function of the Multiblog plugin.
+
+2) Movable Type already provides the IncludeBlogs and ExcludeBlogs
+configuration directives to specify blogs to be included or excluded in a
+search.
+
+3) Multiblog plugin privacy settings shouldn't interact with or override the
+IncludeBlogs / ExcludeBlogs configuration directives.  That could lead to 
+unexpected results, particularly if the settings conflict with each other.
+
+4) Altering the functionality as proposed would make troubleshooting issues 
+with the inclusion / exclusion of blogs in searches much more difficult.  
+There would be no clear way for Movable Type to show that Multiblog 
+privacy settings influence the search results, or that Multiblog privacy 
+settings may be overriding explicit IncludeBlogs / ExcludeBlogs settings 
+in the mt-config.cgi configuration file.
+
+=item 
+
+=back
+
+=head1 AUTHOR & COPYRIGHTS
+
+Author(s): Byrne Reese <byrne@endevver.com>
+
+(C) 2010 Open Melody Software Group.
+
+=cut
