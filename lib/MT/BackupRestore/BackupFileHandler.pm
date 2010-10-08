50a3867c39668ccd40d8961c892370d0c19c0ff7
Byrne <byrne@majordojo.com>
2010-10-08 09:40:42 -0700

    [#236 state:committed] Fixed bad sort operator. The operator being used was a numeric sort operator. However the entities being sorted were strings. Switch to the 'cmp' operator.

--- lib/MT/BackupRestore/BackupFileHandler.pm
+++ lib/MT/BackupRestore/BackupFileHandler.pm
@@ -319,8 +319,8 @@ sub end_element {
             elsif ('role' eq $name) {
                 my $role = $class->load( { name => $obj->name } );
                 if ( $role ) {
-                    my $old_perms = join '', sort { $a <=> $b } split(',', $obj->permissions);
-                    my $cur_perms = join '', sort { $a <=> $b } split(',', $role->permissions);
+                    my $old_perms = join '', sort { $a cmp $b } split(',', $obj->permissions);
+                    my $cur_perms = join '', sort { $a cmp $b } split(',', $role->permissions);
                     if ($old_perms eq $cur_perms) {
                         $self->{objects}->{"$class#$old_id"} = $role;
                         $exists = 1;
