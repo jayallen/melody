f1a97ce56cc4f969cde156c14cd32cefa3ea9907
Byrne <byrne@majordojo.com>
2010-10-14 16:55:53 -0700

    [#391 state:resolved] Mixi Comment is not a service widely utilized. We will refactor this component out into a separate repos to maintain independently.

--- plugins/mixiComment/lib/mixiComment/L10N.pm
+++
@@ -1,5 +0,0 @@
-package mixiComment::L10N;
-use strict;
-use base 'MT::Plugin::L10N';
-
-1;
--- plugins/mixiComment/lib/mixiComment/L10N/en_us.pm
+++
@@ -1,9 +0,0 @@
-package mixiComment::L10N::en_us;
-
-use strict;
-
-use base 'mixiComment::L10N';
-use vars qw( %Lexicon );
-%Lexicon = ();
-
-1;
