de15f2ebf1e92779e3816e9ae56fc00f0a3639d0
Mike <mikerthomsen@gmail.com>
2010-11-03 16:20:43 -0400

    [#505 state:resolved] Edited MultiBlog::Melody to switch $app->param calls to $app->query->param calls.

--- addons/MultiBlog.plugin/lib/MultiBlog/Melody.pm
+++ addons/MultiBlog.plugin/lib/MultiBlog/Melody.pm
@@ -5,16 +5,17 @@ use strict;
 sub search_blog_filter {
     my ( $cb, $app, $list, $processed ) = @_;
     my $plugin = MT->component('MultiBlog');
+    my $q = $app->query;
 
     require MT::Template::Context;
     my $ctx = MT::Template::Context->new;
-    $ctx->stash('blog_id',$app->param('blog_id'));
+    $ctx->stash('blog_id',$q->param('blog_id'));
 
     my $args;
-    $args->{'include_blogs'} = $app->param('IncludeBlogs')
-        if $app->param('IncludeBlogs');
-    $args->{'exclude_blogs'} = $app->param('ExcludeBlogs')
-        if $app->param('ExcludeBlogs');
+    $args->{'include_blogs'} = $q->param('IncludeBlogs')
+        if $q->param('IncludeBlogs');
+    $args->{'exclude_blogs'} = $q->param('ExcludeBlogs')
+        if $q->param('ExcludeBlogs');
 
     require MultiBlog;
     MultiBlog::filter_blogs_from_args($plugin,$ctx,$args);
@@ -23,7 +24,7 @@ sub search_blog_filter {
     if ($args->{'exclude_blogs'}) {
         $key = 'ExcludeBlogs';
         @blogs = split(',',$args->{'exclude_blogs'});
-        $app->param('ExcludeBlogs',$args->{'exclude_blogs'});
+        $q->param('ExcludeBlogs',$args->{'exclude_blogs'});
     } elsif ($args->{'include_blogs'}) {
         $key = 'IncludeBlogs';
         @blogs = split(',',$args->{'include_blogs'});
