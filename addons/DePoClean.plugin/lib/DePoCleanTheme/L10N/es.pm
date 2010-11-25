512ced628d53f2929eca34d9a10560997824c0f2
Mike <mikerthomsen@gmail.com>
2010-11-24 19:13:19 -0500

    [#553 state:resolved] Moved the spanish translation of DePoClean from MT::L10N to DePoClean::L10N.

---
+++ addons/DePoClean.plugin/lib/DePoCleanTheme/L10N/es.pm
@@ -0,0 +1,127 @@
+package DePoCleanTheme::L10N::es;
+use strict;
+use DePoCleanTheme::L10N;
+use DePoCleanTheme::L10N::en_us;
+use vars qw( @ISA %Lexicon );
+@ISA = qw( DePoCleanTheme::L10N::en_us );
+
+## The following is the translation table.
+
+%Lexicon = (
+
+
+## addons/DePoClean.plugin/templates/search_results.mtml
+    'Header' => 'Cabecera',
+    'Search Results for' =>
+      'Resultados de la busqueda para',    # Translate - New
+    'Entry Details' => 'Detalles de la entrada',
+    'Footer'        => 'Pie',
+
+## addons/DePoClean.plugin/templates/flickr.mtml
+    'To show a Flickr user gallery, you must specify a Flickr user ID in Theme Options.'
+      => 'Para mostrar la galer√≠a de usuario. debe especificar un ID de usuario de Flickr en las opciones del tema.'
+    ,                                      # Translate - New
+    'To show a Flickr photoset gallery, you must specify a Flickr user ID in Theme Options.'
+      => 'Para mostrar la galer√≠a de fotos. debe especificar un ID de usuario de Flickr en las opciones del tema.'
+    ,                                      # Translate - New
+    'To show a Flickr group gallery, you must specify a Flickr group ID in Theme Options.'
+      => 'Para mostrar la galer√≠a de grupo. debe especificar un ID de usuario de Flickr en las opciones del tema.'
+    ,                                      # Translate - New
+    'To show a Flickr favorites gallery, you must specify a Flickr user ID in Theme Options.'
+      => 'Para mostrar la galer√≠a de favoritos. debe especificar un ID de usuario de Flickr en las opciones del tema.'
+    ,                                      # Translate - New
+    'View All' => 'Ver Todo',              # Translate - New
+
+## addons/DePoClean.plugin/templates/authors.mtml
+    'View All Archives' => 'Ver toddos los archivos',    # Translate - New
+
+## addons/DePoClean.plugin/templates/entry_listing.mtml
+
+## addons/DePoClean.plugin/templates/feed_entry.mtml
+    'Comments for [_1]'         => 'Comentarios para [_1]',
+    'Comment from [_1] on [_2]' => 'Comentarios de [_1] en [_2]',
+
+## addons/DePoClean.plugin/templates/about.mtml
+    'About' => 'Sobre mi',
+
+## addons/DePoClean.plugin/templates/entry.mtml
+
+## addons/DePoClean.plugin/templates/recent_archives.mtml
+    'Recent Archives' => 'Archivos Recientes',
+
+## addons/DePoClean.plugin/templates/contact.mtml
+
+## addons/DePoClean.plugin/templates/footer.mtml
+    'Powered by' => 'Powered By',    # Translate - Case
+    'using the'  => 'usando el',     # Translate - New
+    'DePo Clean Theme for Melody' =>
+      'Tema DePo Clean para Melody',    # Translate - New
+
+## addons/DePoClean.plugin/templates/entry_details.mtml
+    'Posted'            => 'Publicado',                      # Translate - New
+    'Tagged'            => 'Etiquetado',                     # Translate - New
+    'View all posts in' => 'Ver todas las publicaciones en', # Translate - New
+    'Comment on'        => 'Comentar en',                    # Translate - New
+    'No Comments Yet'   => 'No hay comentarios aun',         # Translate - New
+    'Permanent Link to' => 'Enlace permanente a',            # Translate - New
+
+## addons/DePoClean.plugin/templates/comment_response.mtml
+
+## addons/DePoClean.plugin/templates/categories.mtml
+
+## addons/DePoClean.plugin/templates/dynamic_error.mtml
+
+## addons/DePoClean.plugin/templates/recent_comments.mtml
+
+## addons/DePoClean.plugin/templates/archives.mtml
+    'Archived'     => 'archivada',                           # Translate - New
+    'The Archives' => 'Los Archivos',                        # Translate - New
+    'By Month'     => 'Por Mes',                             # Translate - New
+    'By Category'  => 'Por Categor√≠a',                      # Translate - New
+    'View all posts filed under' =>
+      'Ver todas las entradas bajo',                         # Translate - New
+
+## addons/DePoClean.plugin/templates/main_index.mtml
+    'Flickr' => 'Flickr',
+
+## addons/DePoClean.plugin/templates/action_streams.mtml
+    'Recent Activity' => 'Actividad Reciente',
+
+## addons/DePoClean.plugin/templates/recent_entries.mtml
+
+## addons/DePoClean.plugin/config.yaml
+    'DePoClean\''            => 'DePoClean\'',
+    'Listing Widgets'        => 'Listado de Widgets',
+    'Per Page Entry Limits'  => 'Limites por entrada y p√°gina',
+    'User and Site Metadata' => 'Metadatos del usuario y sitio',
+    'Monthly Archive Limit'  => 'L√≠mite del archivo mensual',
+    'Recent Comments Limit'  => 'L√≠mite de comentarios recientes',
+    'Recent Entries Limit'   => 'L√≠mite de entradas recientes',
+    'Type of photo display'  => 'Tipo de foto a mostrar',
+    'User ID'                => 'ID de usuario',
+    'Group ID'               => 'ID de Grupo',
+    'Photoset ID'            => 'Photoset ID',
+    'Number of comments in comment feed' =>
+      'N√∫mero de comentarios en la feed de comentarios',
+    'Number of entries on the main page' =>
+      'N√∫mero de entradas en la p√°gina principal',
+    'Number of entries on the listing pages' =>
+      'N√∫mero de entradas en el listado de p√°ginas',
+    'Number of entries in the search results' =>
+      'N√∫mero de entradas en los resultados de la b√∫squeda',
+    'About You'                     => 'Acerca de Usted',
+    'Contact Information'           => 'Informaci√≥n de contacto',
+    'Primary Author'                => 'Autor primario',
+    'Atom Feed'                     => 'Atom Feed',
+    'MT jQuery Data'                => 'MT jQuery Data',
+    'RSS Feed'                      => 'RSS Feed',
+    'RSS Feed (comments)'           => 'RSS Feed (comments)',
+    'Sitemap'                       => 'Mapa del sitio',
+    'Comment Feeds'                 => 'Feed de comentarios',
+    'Action Streams'                => 'Torrente de acciones',
+    'DePoClean Theme Preferences\'' => 'Preferencias del tema de DepoClean',
+
+);
+
+1;
+
