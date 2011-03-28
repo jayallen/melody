af8d038cbe7b52064ecfc6d3dc691b0a33af7012
Jay Allen <jay@endevver.com>
2011-03-28 16:28:03 -0700

    [#570] Amended previous commit to convert dash in url parameter to our standard underscore

--- lib/MT/CMS/Blog.pm
+++ lib/MT/CMS/Blog.pm
@@ -1059,10 +1059,13 @@ sub start_rebuild_pages {
     my $start_time = $q->param('start_time');
 
     if ( !$start_time ) {
-        #If the user selected the purge cache checkbox, purge all cached objects
-        if ( $q->param('purge-cache')) {
+        # If purge_cache checkbox is checked, purge all cached objects
+        if ( $q->param('purge_cache')) {
             my $blog_id = $app->blog->id;
-            MT->model('session')->remove({ id => { like => 'blog::' . $blog_id }, kind => 'CO' });
+            MT->model('session')->remove({
+                id   => { like => 'blog::' . $blog_id },
+                kind => 'CO',
+            });
         }
         # start of build; invoke callback
         $app->run_callbacks('pre_build');
